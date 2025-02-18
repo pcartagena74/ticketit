f4676d24bf665e71580247e09cfb051c41af6a40
Phil <register@fierce.net>
2025-02-17 21:32:19 -0500

    Update create.blade.php
    Removed references to CollectiveForm

--- src/Views/bootstrap3/tickets/create.blade.php
+++ src/Views/bootstrap3/tickets/create.blade.php
@@ -4,49 +4,57 @@
 @section('content')
 @include('ticketit::shared.header')
     <div class="well bs-component">
-        {!! CollectiveForm::open([
+        {{--
+        {!! html()->open([
                         'route'=>$setting->grab('main_route').'.store',
                         'method' => 'POST',
                         'class' => 'form-horizontal'
                         ]) !!}
+        --}}
+        {!! html()->form(
+            'POST',
+            $setting->grab('main_route').'.store',
+            'form-horizontal'
+        )->open()
+        !!}
             <legend>{!! trans('ticketit::lang.create-new-ticket') !!}</legend>
             <div class="form-group">
-                {!! CollectiveForm::label('subject', trans('ticketit::lang.subject') . trans('ticketit::lang.colon'), ['class' => 'col-lg-2 control-label']) !!}
+                {!! html()->label('subject', trans('ticketit::lang.subject') . trans('ticketit::lang.colon'), ['class' => 'col-lg-2 control-label']) !!}
                 <div class="col-lg-10">
-                    {!! CollectiveForm::text('subject', null, ['class' => 'form-control', 'required' => 'required']) !!}
+                    {!! html()->text('subject', null, ['class' => 'form-control', 'required' => 'required']) !!}
                     <span class="help-block">{!! trans('ticketit::lang.create-ticket-brief-issue') !!}</span>
                 </div>
             </div>
             <div class="form-group">
-                {!! CollectiveForm::label('content', trans('ticketit::lang.description') . trans('ticketit::lang.colon'), ['class' => 'col-lg-2 control-label']) !!}
+                {!! html()->label('content', trans('ticketit::lang.description') . trans('ticketit::lang.colon'), ['class' => 'col-lg-2 control-label']) !!}
                 <div class="col-lg-10">
-                    {!! CollectiveForm::textarea('content', null, ['class' => 'form-control summernote-editor', 'rows' => '5', 'required' => 'required']) !!}
+                    {!! html()->textarea('content', null, ['class' => 'form-control summernote-editor', 'rows' => '5', 'required' => 'required']) !!}
                     <span class="help-block">{!! trans('ticketit::lang.create-ticket-describe-issue') !!}</span>
                 </div>
             </div>
             <div class="form-inline row">
                 <div class="form-group col-lg-4">
-                    {!! CollectiveForm::label('priority', trans('ticketit::lang.priority') . trans('ticketit::lang.colon'), ['class' => 'col-lg-6 control-label']) !!}
+                    {!! html()->label('priority', trans('ticketit::lang.priority') . trans('ticketit::lang.colon'), ['class' => 'col-lg-6 control-label']) !!}
                     <div class="col-lg-6">
-                        {!! CollectiveForm::select('priority_id', $priorities, null, ['class' => 'form-control', 'required' => 'required']) !!}
+                        {!! html()->select('priority_id', $priorities, null, ['class' => 'form-control', 'required' => 'required']) !!}
                     </div>
                 </div>
                 <div class="form-group col-lg-4">
-                    {!! CollectiveForm::label('category', trans('ticketit::lang.category') . trans('ticketit::lang.colon'), ['class' => 'col-lg-6 control-label']) !!}
+                    {!! html()->label('category', trans('ticketit::lang.category') . trans('ticketit::lang.colon'), ['class' => 'col-lg-6 control-label']) !!}
                     <div class="col-lg-6">
-                        {!! CollectiveForm::select('category_id', $categories, null, ['class' => 'form-control', 'required' => 'required']) !!}
+                        {!! html()->select('category_id', $categories, null, ['class' => 'form-control', 'required' => 'required']) !!}
                     </div>
                 </div>
-                {!! CollectiveForm::hidden('agent_id', 'auto') !!}
+                {!! html()->hidden('agent_id', 'auto') !!}
             </div>
             <br>
             <div class="form-group">
                 <div class="col-lg-10 col-lg-offset-2">
                     {{ html()->a($setting->grab('main_route') . '.index', trans('ticketit::lang.btn-back'))->class('btn btn-default') }}
-                    {!! CollectiveForm::submit(trans('ticketit::lang.btn-submit'), ['class' => 'btn btn-primary']) !!}
+                    {!! html()->submit(trans('ticketit::lang.btn-submit'), ['class' => 'btn btn-primary']) !!}
                 </div>
             </div>
-        {!! CollectiveForm::close() !!}
+        {!! html()->form()->close() !!}
     </div>
 @endsection
 
