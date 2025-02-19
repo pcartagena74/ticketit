46206363fc91aebf413a7965b14c7551a6e93061
Phil <register@fierce.net>
2025-02-19 18:58:12 -0500

    Update comment_form.blade.php
    final changes to address additional parameters to form fields

--- src/Views/bootstrap3/tickets/partials/comment_form.blade.php
+++ src/Views/bootstrap3/tickets/partials/comment_form.blade.php
@@ -14,12 +14,19 @@
                 <legend>{!! trans('ticketit::lang.reply') !!}</legend>
                 <div class="form-group">
                     <div class="col-lg-12">
-                        {!! html()->textarea('content', null, ['class' => 'form-control summernote-editor', 'rows' => "3"]) !!}
+                    {!!
+                        html()->textarea('content', null)
+                              ->class(['form-control', 'summernote-editor'])
+                              ->rows(3)
+                    !!}
                     </div>
                 </div>
 
                 <div class="text-right col-md-12">
-                    {!! html()->submit( trans('ticketit::lang.btn-submit'), ['class' => 'btn btn-primary']) !!}
+                    {!! 
+                        html()->submit( trans('ticketit::lang.btn-submit'))
+                              ->class(['btn btn-primary']) 
+                    !!}
                 </div>
 
             </fieldset>
